7bdb8c3353fa87fd436cc49a6f99509fa61862e1
arunachalamev <arunachalamev@gmail.com>
2017-07-17 11:50:28 +0530

    auc calculation

---
+++ AUC.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 11:32:50 2017
+
+@author: arellave
+"""
+
+import numpy as np
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score
+
+#Given x, y real value, generate the auc
+y = np.array([1, 1, 2, 2])
+pred = np.array([0.1, 0.4, 0.35, 0.8])
+fpr, tpr, thresholds = metrics.roc_curve(y, pred, pos_label=2)
+print metrics.auc(fpr, tpr)
+
+#Given true and score value, generate the auc
+y_true = np.array([0, 0, 1, 1])
+y_scores = np.array([0.1, 0.4, 0.35, 0.8])
+print roc_auc_score(y_true, y_scores)
+
