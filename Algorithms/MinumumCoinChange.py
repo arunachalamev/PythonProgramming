a0ea6f620e8960f0066a05720875ad7500f1d78a
arunachalamev <arunachalamev@gmail.com>
2017-07-18 21:47:57 +0530

    Coin Exchange

--- Algorithms/MinumumCoinChange.py
+++ Algorithms/MinumumCoinChange.py
@@ -18,5 +18,18 @@ def MinimumCoinChange(V):
             res = sub_residual + 1
     return res
 
+
+def min_change(V, C):
+    m, n = len(V)+1, C+1
+    table = [[0] * n for x in xrange(m)]
+    for j in xrange(1, n):
+        table[0][j] = float('inf')
+    for i in xrange(1, m):
+        for j in xrange(1, n):
+            aC = table[i][j - V[i-1]] if j - V[i-1] >= 0 else float('inf')
+            table[i][j] = min(table[i-1][j], 1 + aC)
+    return table[m-1][n-1]
+    
+    
 if __name__ == "__main__":
-    print MinimumCoinChange(15)
+    print min_change([1,2,3],10)    
