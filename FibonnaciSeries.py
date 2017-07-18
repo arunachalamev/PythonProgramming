bad64bf6a2009063b28d3f44b5efe11313968d3a
arunachalamev <arunachalamev@gmail.com>
2017-07-18 15:49:15 +0530

    LCS and Fibonnaci

---
+++ FibonnaciSeries.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 12:18:48 2017
+
+@author: arellave
+"""
+#%%
+n =10
+Fib=[]
+
+Fib.append(0)
+Fib.append(1)
+Fib.append(1)
+
+for i in range(3,n+1):
+    FirstElement = Fib[i-2]
+    SecondElement = Fib[i-1]
+    Fib.append(FirstElement+SecondElement)
+
+print Fib
+#%%
---
+++ LCS.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 14:24:49 2017
+
+@author: arellave
+"""
+
+#%%
+
+FS = "ABC"
+SS = "2222A"
+
+lenFS = len(FS)
+lenSS = len(SS)
+
+LCS = [[0 for j in range(lenSS+1)] for i in range(lenFS+1)]
+
+print LCS
+
+for i in range(lenFS-1,-1,-1):
+    for j in range(lenSS-1,-1,-1):
+        LCS[i][j] = LCS[i+1][j+1]
+        if (FS[i]==SS[j]):
+            LCS[i][j] =  LCS[i][j] +1
+        else:
+            LCS[i][j] = max(LCS[i][j],LCS[i+1][j],LCS[i][j+1])
+
+print LCS
+print LCS[0][0]
+                
+
+#%%
