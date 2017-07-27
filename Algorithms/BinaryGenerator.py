a9c31af64305ef22f51fd50db88f5bd92a1fc0e3
arunachalamev <arunachalamev@gmail.com>
2017-07-27 14:57:13 +0530

    Generates Binary number using recursion

---
+++ Algorithms/BinaryGenerator.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 14:47:30 2017
+
+@author: arellave
+"""
+
+#To generate Binary strings of given length
+n=3
+B = [-1]*n
+
+def Binary(n):
+    global B
+    if (n<=0):
+        print B
+        return
+    B[n-1]=0
+    Binary(n-1)
+    B[n-1]=1
+    Binary(n-1)
+    
+Binary(n)
