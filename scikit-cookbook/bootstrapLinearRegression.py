5fd1ab36d01ed1eb5760ecdd82ddc936030b9db8
arunachalamev <arunachalamev@gmail.com>
2017-08-17 08:04:14 +0530

    Linear regression with boston dataset

---
+++ scikit-cookbook/bootstrapLinearRegression.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 20:00:26 2017
+
+@author: arellave
+"""
+
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+lr = LinearRegression()
+
+boston = datasets.load_boston()
+
+n_bootstraps = 1000
+len_boston = len(boston.target)
+
+subsample_size = np.int(0.5*len_boston)
+
+#Lambda function
+subsample = lambda:np.random.choice(np.arange(0,len_boston),size=subsample_size)
+
+coefs = np.ones(n_bootstraps)
+
+for i in range (n_bootstraps):
+    subsample_idx = subsample()
+    subsample_X = boston.data[subsample_idx]
+    subsample_y = boston.target[subsample_idx]
+    lr.fit(subsample_X,subsample_y)
+    coefs[i] = lr.coef_[0]
+    
+import matplotlib.pyplot as plt
+f = plt.figure(figsize=(7,5))
+ax = f.add_subplot(111)
+ax.hist(coefs,bins=50)
+ax.set_title("Histogram of the lr.coef_[0].")
+
+print np.percentile(coefs,[2.5,97.5])
---
+++ scikit-cookbook/linearRegression.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 16 19:34:13 2017
+
+@author: arellave
+"""
+
+from sklearn import datasets
+boston = datasets.load_boston()
+
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+
+lr.fit(boston.data,boston.target)
+
+predictions = lr.predict(boston.data)
+
+print "Printing Coefficient :"
+print lr.coef_
+
+print "Coefficient along with feature Name :"
+print zip(boston.feature_names,lr.coef_)
+
+
+#Fitting by normalizing the data
+lr2 = LinearRegression(normalize= True)
+lr2.fit(boston.data,boston.target)
+preiction2 = lr2.predict(boston.data)
+
+print "Coefficient along with feature Name :"
+print zip(boston.feature_names,lr2.coef_)
+
+
+#Plotting the residuals
+import matplotlib.pyplot as plt
+import numpy as np
+f= plt.figure(figsize=(7,5))
+ax = f.add_subplot(111)
+ax.hist(boston.target-predictions,bins=50)
+ax.set_title("Histogram of Residuals")
+
+print "Mean of residuals :"
+print np.mean(boston.target-predictions)
+
+
+#plotting q-q plot
+from scipy.stats import probplot
+f= plt.figure(figsize=(7,5))
+ax = f.add_subplot(111)
+probplot(boston.target-predictions,plot=ax)
+
+
+
+def MSE(target, predictions):
+    squared_deviation = np.power(target-predictions,2)
+    return np.mean(squared_deviation)
+    
+    
+def MAD(target,predictions):
+    absolute_deviation = np.abs(target-predictions)
+    return np.mean(absolute_deviation)
+    
+print "Mean squared Error :"    
+print MSE(boston.target,predictions)
+
+print "Mean Absolute Error :"    
+print MAD(boston.target,predictions)
+
