7944eca6ff9cb9a3d514a5d73d7d2ca98586a60b
arunachalamev <arunachalamev@gmail.com>
2017-08-23 21:11:34 +0530

    Load data from a file line by line

---
+++ MLinAction/Chapter8-Regression/loadDatasetFromFile.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 22 19:30:38 2017
+
+@author: arellave
+"""
+
+from numpy import mat, linalg
+
+def loadDataSet(fileName):
+    numFeat = len(open(fileName).readline().split('\t'))-1
+    dataMat = []; labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat,labelMat
+
+def standRegress(xArr,yArr):
+    xMat = mat(xArr); yMat = mat(yArr).T
+    xTx = xMat.T*xMat
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    ws = xTx.I*(xMat.T*yMat)
+    return ws
+    
