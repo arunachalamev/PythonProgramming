2f96f39e747c2c5e8ca3bc64a6557396fa405731
arunachalamev <arunachalamev@gmail.com>
2019-10-21 16:02:39 -0700

    Included second version

--- Algorithms/removeSubFolders.py
+++ Algorithms/removeSubFolders.py
@@ -1,23 +1,25 @@
 
-# def removeSubfolders(folder):
-#     folder.sort()
-#     res = [folder[0]]
+# Version 1
+def removeSubfolders1(folder):
+    folder.sort()
+    res = [folder[0]]
 
-#     prev_folder = folder[0] 
-#     prev_folder_slash = prev_folder +'/'
-#     prev_folder_n = len(prev_folder_slash)
-#     for i in range (1,len(folder)):
-#         if ((folder[i])[:prev_folder_n] == prev_folder_slash):
-#             pass
-#         else:
-#             res.append(folder[i])
-#             prev_folder = folder[i]
-#             prev_folder_slash = prev_folder +'/'
-#             prev_folder_n = len(prev_folder_slash)
+    prev_folder = folder[0] 
+    prev_folder_slash = prev_folder +'/'
+    prev_folder_n = len(prev_folder_slash)
+    for i in range (1,len(folder)):
+        if ((folder[i])[:prev_folder_n] == prev_folder_slash):
+            pass
+        else:
+            res.append(folder[i])
+            prev_folder = folder[i]
+            prev_folder_slash = prev_folder +'/'
+            prev_folder_n = len(prev_folder_slash)
 
-#     return res
+    return res
 
-def removeSubfolders(folder):
+# Version 2
+def removeSubfolders2(folder):
     folder.sort()
     ans = []
     for f in folder:
@@ -26,7 +28,7 @@ def removeSubfolders(folder):
     return ans
 
 
-print(removeSubfolders(["/a","/a/b","/c/d","/c/d/e","/c/f"]))
+print(removeSubfolders1(["/a","/a/b","/c/d","/c/d/e","/c/f"]))
 # print(removeSubfolders(["/a","/a/b/c","/a/b/d"]))
 # print(removeSubfolders(["/a/b/c","/a/b/ca","/a/b/d"]))
 # print(removeSubfolders(["/abc/de","/abc/dee","/abc/def","/abc/def/gh","/abc/def/ghi","/abc/def/ghij","/abc/def/ghijk","/abc/dez"]))
