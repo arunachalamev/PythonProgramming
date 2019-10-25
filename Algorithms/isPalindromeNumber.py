56f9ac802e29fb0934718562ea7aedbcdd484bf8
arunachalamev <arunachalamev@gmail.com>
2019-10-24 18:20:38 -0700

    Check whether the given number is  palindrome without converting into string

---
+++ Algorithms/isPalindromeNumber.py
@@ -0,0 +1,14 @@
+def isPalindrome(x):
+    input = x
+    if x < 0:
+        return False
+
+    reverseNumber = 0
+    while (x>0):
+        reverseNumber = reverseNumber * 10 + x %10
+        x = x//10
+    
+    if input == reverseNumber : return True
+    else: return False
+
+print(isPalindrome(10))
