008866df771eb9f5e839205962c8df54ab84b546
arunachalamev <arunachalamev@gmail.com>
2019-10-24 18:58:25 -0700

    find longest common prefix among all strings

--- Algorithms/longestCommonPrefix.py
+++ Algorithms/longestCommonPrefix.py
@@ -1,7 +1,13 @@
 def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+    minstr = min(strs,key = len)
+    for i,ch in enumerate(minstr):
+        for others in strs:
+            if others[i] != ch:
+                return minstr[:i]
+    return minstr
 
-
-    return None
 
 print (longestCommonPrefix(["flower","flow","flight"]))
 print (longestCommonPrefix(["dog","racecar","car"]))
