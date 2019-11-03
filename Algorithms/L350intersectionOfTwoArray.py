5373b5ce7db3eca371d67ea01f8e07628ca92f0a
arunachalamev <arunachalamev@gmail.com>
2019-11-03 09:57:14 -0800

    intersection of two arrays

---
+++ Algorithms/L350intersectionOfTwoArray.py
@@ -0,0 +1,29 @@
+# Find intersection of two arrays
+
+def intersectionOfTwoArray(x,y):
+
+    if len(x) > len(y):
+        x,y = y, x
+
+    x.sort()
+    y.sort()
+    
+    i,j = 0,0
+    result = []
+
+    while (i<len(x) and j < len(y)):
+        if x[i] == y[j]:
+            result.append(x[i])
+            i = i +1
+            j = j +1
+        elif x[i] < y[j] :
+            i = i + 1
+        elif x[i] > y[j]:
+            j = j + 1
+
+    print (result)
+
+    return  result
+
+intersectionOfTwoArray([1,2,3,3,4],[1,2,3,3,5,6,7,9])
+
