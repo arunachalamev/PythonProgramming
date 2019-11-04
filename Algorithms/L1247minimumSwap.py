66ec262d8e4cc1256836ec939a51030e09c1e95a
arunachalamev <arunachalamev@gmail.com>
2019-11-03 19:05:13 -0800

    added description

--- Algorithms/L1247minimumSwap.py
+++ Algorithms/L1247minimumSwap.py
@@ -1,3 +1,10 @@
+# You are given two strings s1 and s2 of equal length consisting of letters "x" and "y" only. 
+# Your task is to make these two strings equal to each other. 
+# You can swap any two characters that belong to different strings, which means: swap s1[i] and s2[j].
+
+# Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if it is impossible to do so.
+
+
 def minimumSwap(s1,s2):
     countX, countY = 0, 0
     mismatch = 0
