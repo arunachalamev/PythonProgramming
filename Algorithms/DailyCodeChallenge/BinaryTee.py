06c5d9288c84249167de10ac4318e913b7392f84
arunachalamev <arunachalamev@gmail.com>
2019-11-05 09:59:16 -0800

    Create Binary Tree

---
+++ Algorithms/DailyCodeChallenge/BinaryTee.py
@@ -0,0 +1,14 @@
+class Node:
+	def __init__(self, val, left=None, right=None):
+		self.val = val
+		self.left = left
+		self.right = right
+
+tree = Node(4, Node(2, Node(1), Node(3)), Node(5))
+
+
+#     4
+#    / \
+#   2   5
+#  / \  
+# 1   3
