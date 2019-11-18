16f5f45267ef1641d2f70ff7aa0905ad4241b2d6
arunachalamev <arunachalamev@gmail.com>
2019-11-17 19:39:36 -0800

    renaming the file name to suit email list

---
+++ Algorithms/DailyCodeChallenge/DC20191110longestSubstring.py
@@ -0,0 +1,8 @@
+# Given an integer k and a string s, find the length of the longest substring that contains at most k distinct characters.
+
+# For example, given s = "abcba" and k = 2, the longest substring with k distinct characters is "bcb".
+
+def longestSubstring (s,k):
+
+
+print (longestSubstring('abcba',2))
