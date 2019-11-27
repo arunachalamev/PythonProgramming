f4bddf54eb000c4d0b22acb5db10e4dd7d1f7a47
arunachalamev <arunachalamev@gmail.com>
2019-11-27 15:24:55 -0800

    added comments

--- Algorithms/DailyCodeChallenge/CompleteBinaryTreeInArrayToSortedOrder.py
+++ Algorithms/DailyCodeChallenge/CompleteBinaryTreeInArrayToSortedOrder.py
@@ -1,3 +1,5 @@
+# Given an array that stores a complete Binary Search Tree, write a function that efficiently prints the given array in ascending order.
+# For example, given an array [4, 2, 5, 1, 3], the function should print 1, 2, 3, 4, 5
 
 def completeBinaryTreeInArrayToSortedOrder(A,start,end):
 
