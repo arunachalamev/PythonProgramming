424de889c39b720bc1aa1a3ceb24975bf24ff5b3
arunachalamev <arunachalamev@gmail.com>
2019-11-28 12:27:34 -0800

    adding memoziation

--- Algorithms/DailyCodeChallenge/DC031editDistance.py
+++ Algorithms/DailyCodeChallenge/DC031editDistance.py
@@ -12,4 +12,25 @@ def editDistance(s1,s2):
     if s1[0] == s2[0]: return editDistance(s1[1:],s2[1:])
     return 1 + min (editDistance(s1[1:],s2) , editDistance(s1,s2[1:]), editDistance(s1[1:],s2[1:]))
 
-print (editDistance("sitting","kitten"))
+# using cache
+def editDistance2(s1,s2):
+    m,n = len(s1), len(s2)
+    
+    table = [[0]*(n+1) for _ in range(m+1)]
+
+    for j in range(n+1):
+        table[0][j] = j 
+    for i in range(m+1):
+        table[i][0] = i
+
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if s1[i-1] == s2[j-1]:
+                table[i][j] = table[i-1][j-1]
+            else:
+                table[i][j] = 1 + min (table[i-1][j-1], table[i-1][j], table[i][j-1])
+    
+    return table[-1][-1]
+
+
+print (editDistance2("sitting","kitten"))
