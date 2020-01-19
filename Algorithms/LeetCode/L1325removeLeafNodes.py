f56158a1760b0d2d93ade403bc52b18ddc9c54a8
arunachalamev <arunachalamev@gmail.com>
2020-01-19 12:45:12 -0800

    Revert "remove leafnode with target value"
    This reverts commit 2b72610d46006fd42b9f68b8aa204d3069f422e2.

--- Algorithms/LeetCode/L1325removeLeafNodes.py
+++
@@ -1,9 +0,0 @@
-
-
-def removeLeafNodes(node,target):
-    if node:
-        node.left = removeLeafNodes(node.left, target)
-        node.right = removeLeafNodes(node.right, target)
-        if node.val == target and node.left is None and node.right is None:
-            return None
-        return node
